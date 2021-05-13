05cd47c735455b5d4bd09ed9b0d154ab4d377e38
Yaroslav Yefimtsev <yefimtsev.y@gmail.com>
2021-05-13 21:12:15 +0300

    Add various improvements (read inside)
    This commit adds basic approach to update and determine nodes weights.
    Also it add method printHumanReadableNodes which allows to print weights nicely.

--- src/com/github/kpi/branchnbounds/ProblemSolver.java
+++ src/com/github/kpi/branchnbounds/ProblemSolver.java
@@ -1,16 +1,12 @@
 package com.github.kpi.branchnbounds;
 
 import java.util.*;
+import java.util.stream.Collectors;
+
 // a = 0, g = 6
 public class ProblemSolver {
 
-    public ProblemSolver() {
-        printMatrix();
-//        System.out.println(findChildNodes(0));
-        System.out.println(letterToInteger);
-    }
-
-    int[][] matrix = {
+    private int[][] matrix = {
             {0, 2, 4, 5, 0, 0, 0},
             {0, 0, 1, 0, 5, 12, 0},
             {0, 0, 0, 0, 0, 9, 18},
@@ -19,7 +15,20 @@ public class ProblemSolver {
             {0, 0, 0, 0, 0, 0, 1},
             {0, 0, 0, 0, 0, 0, 0}};
 
-    ArrayList<Integer> final_path = new ArrayList<>();
+    public ProblemSolver() {
+        printMatrix();
+        currentNodeWeights.put(0, 0); // set default node as 0 (a) and it's weight to 0;
+        currentNodeWeights.put(1, 2); // set default node as 1 (b) and it's weight to 2;
+        updateCurrentNodeWeights(0,1);
+        updateCurrentNodeWeights(1, 2);
+        printHumanReadableNodes();
+    }
+
+    private ArrayList<Integer> finalPath = new ArrayList<>();
+    private ArrayList<Integer> currentPath = new ArrayList<>();
+    private HashMap<Integer, Integer> currentNodeWeights = new HashMap<>(); // node index, node weight
+
+    private int currentSize = 0; // current size represents the amount of the connected nodes
 
     public void printMatrix() {
         for (int[] ints : matrix) {
@@ -30,36 +39,42 @@ public class ProblemSolver {
         }
     }
 
-    private Map<Integer, Integer> findChildNodes(int parentNode, int currentNode) {
+    private void printHumanReadableNodes() {
+        currentNodeWeights.forEach((k, v) -> System.out.print(letterToInteger.get(k) + " = " + v + " | "));
+    }
+
+    private Map<Integer, Integer> findChildNodes(int currentNode) {
         Map<Integer, Integer> childNodes= new HashMap<>();
         for (int i = 0; i < matrix.length; i++) {
             if (matrix[currentNode][i] != 0) {
-                childNodes.put(i, calculateNodeWeight(parentNode, currentNode));
+                childNodes.put(i, matrix[currentNode][i]);
+                currentSize++;
             }
         }
 
         return childNodes;
     }
 
-    private int calculateNodeWeight(int parentNode, int currentNode) {
-
-        return 0;
+    private void updateCurrentNodeWeights(int parentNode, int currentNode) {
+        Map<Integer, Integer> tempMap = findChildNodes(currentNode);
+        tempMap.forEach((k, v) -> currentNodeWeights.put(k, v + currentNodeWeights.get(currentNode)));
     }
 
-    private final HashMap<String, Integer> letterToInteger = createHashMap();
 
 
-    private HashMap<String, Integer> createHashMap() {
-        HashMap<String, Integer> temp = new HashMap<>();
-        temp.put("a", 0);
-        temp.put("b", 1);
-        temp.put("c", 2);
-        temp.put("d", 3);
-        temp.put("e", 4);
-        temp.put("f", 5);
-        temp.put("g", 6);
-        temp.put("h", 7);
-        temp.put("i", 8);
+    private final HashMap<Integer, String> letterToInteger = createHashMap();
+
+    private HashMap<Integer, String> createHashMap() {
+        HashMap<Integer, String> temp = new HashMap<>();
+        temp.put(0, "a");
+        temp.put(1, "b");
+        temp.put(2, "c");
+        temp.put(3, "d");
+        temp.put(4, "e");
+        temp.put(5, "f");
+        temp.put(6, "g");
+        temp.put(7, "h");
+        temp.put(8, "i");
         return temp;
     }
 }
